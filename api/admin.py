aa0f82fa290695a7baae42a243f4a0df02fed64f
Emmanuel <emmapraise@gmail.com>
2022-05-21 00:51:39 +0100

    feat: admin models created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from api.models import Admin, User
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register([Admin])
--- api/models.py
+++ api/models.py
@@ -18,3 +18,26 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
+
+
+class common(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Admin(common):
+    ADMIN_TYPE = (
+        ("Local", "Local Goverment"),
+        ("State", "State Goverment"),
+        ("Federal", "Federal Goverment"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    admin_type = models.CharField(
+        max_length=10, choices=ADMIN_TYPE, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.user
