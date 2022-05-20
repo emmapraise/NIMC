89c1ca58f9ad97d0347d6a731405180012e0cd71
Emmanuel <emmapraise@gmail.com>
2022-05-21 00:15:42 +0100

    Feat: created user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """This represent the user object on the system"""
+
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+    phone = models.CharField(max_length=11, unique=True)
+    nin = models.CharField(max_length=20, unique=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
+
+    USERNAME_FIELD = "nin"
+
+    def __str__(self):
+        return self.get_full_name()
