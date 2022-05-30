4b5c9ed9c511ca0bc9e17c0c216fd2f5783d4e6c
Emmanuel <emmapraise@gmail.com>
2022-05-31 00:29:49 +0100

    Setup frontend

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<v-btn depressed color="primary" x-large> Click me </v-btn>
+		<v-btn depressed color="error">
+			Primary
+		</v-btn>
+	</div>
+</template>
