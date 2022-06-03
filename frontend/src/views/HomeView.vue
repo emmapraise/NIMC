08a38ab7f62fcbefd2ca0ffb20d85bf91323bf5f
Emmanuel <emmapraise@gmail.com>
2022-06-03 19:37:55 +0100

    feat: created landing page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,34 +1,45 @@
 <template>
-	<b-container fluid class="bv-example-row">
-		<b-row>
-			<b-col>
-				<div>
-					<b-img
-						src="https://picsum.photos/300/150/?image=41"
-						fluid-grow
-						alt="Fluid-grow image"
-					></b-img>
+	<div class="container">
+		<div class="row justify-content-center align-items-center main mx-auto">
+			<div class="col-md-4" role="button" @click="handleClick('quotes')">
+				<div class="card">
+					<div class="d-flex flex-column align-items-center w-100 p-5">
+						<b-icon icon="person-fill"></b-icon>
+						<span style="font-weight: bold; width: fit-content"
+							>User Login</span
+						>
+						<span class="text-secondary"> User Login to Dashboard </span>
+					</div>
 				</div>
-			</b-col>
-			<b-col>
-				<b-button variant="success" size="lg">Sign Up</b-button>
-				<b-button variant="success" size="lg">Login</b-button>
-				<b-button variant="success" size="lg">Check In</b-button>
-			</b-col>
-		</b-row>
-		<!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-	</b-container>
+			</div>
+
+			<div class="col-md-4" role="button" @click="handleClick('admin')">
+				<div class="card">
+					<div class="d-flex flex-column align-items-center w-100 p-5">
+						<b-icon icon="person-plus-fill" aria-hidden="true"></b-icon>
+						<span style="font-weight: bold; width: fit-content">Sign Up</span>
+						<span class="text-secondary">Login or Sign up a new Admin</span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4" role="button" @click="handleClick('admin')">
+				<div class="card">
+					<div class="d-flex flex-column align-items-center w-100 p-5">
+						<b-icon icon="search" aria-hidden="true"></b-icon>
+						<span style="font-weight: bold; width: fit-content">Check In</span>
+						<span class="text-secondary">Check the Validity of a NIN</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
 	name: 'HomeView',
-	components: {
-		// HelloWorld,
-	},
+	components: {},
 	data() {
 		return {
 			text: 'this is it',
