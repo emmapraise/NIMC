2293ccc659a83fb96a1cd93580ac5aa68264860a
Emmanuel <emmapraise@gmail.com>
2022-06-21 00:09:12 +0100

    feat: nin info details added

--- NIMC/enums/nin.py
+++ NIMC/enums/nin.py
@@ -7,8 +7,25 @@ VOTERS_CARD = "Voter’s card (Temporary or Permanent)"
 INTERNATIONAL_PASSPORT = "Nigerian International passport"
 COO = "Certificate of Origin"
 
+# Martial Status
+SINGLE = "Single"
+MARRIED = "Married"
+DIVORCED = "Divoreced"
 
-def document_types():
+# Blood Group
+A = "A"
+B = "B"
+AB = "AB"
+O = "O"
+
+# Genotype
+AA = "AA"
+AS = "AS"
+AC = "AC"
+SS = "SS"
+
+
+def documentTypes():
     types = [
         (OLD_NATIONAL_ID_CARD, "Old National ID Card"),
         (DRIVERS_LICENSE, "Driver’s License"),
@@ -18,3 +35,18 @@ def document_types():
     ]
 
     return types
+
+
+def maritalStatus():
+    type = [(SINGLE, "Single"), (MARRIED, "Married"), (DIVORCED, "Divoreced")]
+    return type
+
+
+def bloodGroup():
+    group = [(A, "A"), (B, "B"), (AB, "AB"), (O, "O")]
+    return group
+
+
+def genotype():
+    type = [(AA, "AA"), (AS, "AS"), (AC, "AC"), (SS, "SS")]
+    return type
