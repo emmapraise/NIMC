239b7002ed956f47e3eb93fdf9930a20bb32c2ac
Emmanuel <emmapraise@gmail.com>
2022-07-04 13:54:50 +0100

    feat: combobox

--- NIMC/settings.py
+++ NIMC/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 50,
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
