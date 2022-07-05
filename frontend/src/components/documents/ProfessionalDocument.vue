13e8631664f3c4cdde5691ab6c2970c2051403c7
Emmanuel <emmapraise@gmail.com>
2022-07-05 23:27:11 +0100

    feat: professional document

--- frontend/src/components/documents/ProfessionalDocument.vue
+++ frontend/src/components/documents/ProfessionalDocument.vue
@@ -1,39 +1,55 @@
 <template>
 	<div>
-		<b-row v-for="(item, index) in professional_tab" :key="index" class="my-2">
-			<b-col sm="3">
-				<label :for="item.label">{{ item.name }}</label></b-col
+		<b-alert
+			:show="dismissCountDown"
+			dismissible
+			variant="success"
+			@dismissed="dismissCountDown = 0"
+			@dismiss-count-down="countDownChanged"
+		>
+			<p>Document Uploaded Successfully</p>
+		</b-alert>
+		<b-form @submit.prevent="onSubmit">
+			<b-row
+				v-for="(item, index) in professional_tab"
+				:key="index"
+				class="my-2"
 			>
-			<b-col sm="9">
-				<div v-if="item.type === 'select'">
-					<b-form-select
-						:id="item.label"
-						v-model="item.value"
-						required
-						:options="item.options"
-					></b-form-select>
-				</div>
-				<div v-else-if="item.type === 'file'">
-					<b-form-file
-						v-model="item.value"
-						:state="Boolean(item.value)"
-						placeholder="Choose a file or drop it here..."
-						drop-placeholder="Drop file here..."
-					></b-form-file>
-				</div>
-				<div v-else-if="item.type === 'combobox'">
-					<user-combobox-vue :nininfo="nininfo" />
-				</div>
-				<div v-else>
-					<b-form-input
-						:id="item.label"
-						:type="item.type"
-						required
-					></b-form-input></div></b-col
-		></b-row>
-		<div class="float-right mt-2">
-			<b-button variant="success">Submit</b-button>
-		</div>
+				<b-col sm="3">
+					<label :for="item.label">{{ item.name }}</label></b-col
+				>
+				<b-col sm="9">
+					<div v-if="item.type === 'select'">
+						<b-form-select
+							:id="item.label"
+							v-model="item.value"
+							required
+							:options="item.options"
+						></b-form-select>
+					</div>
+					<div v-else-if="item.type === 'file'">
+						<b-form-file
+							v-model="item.value"
+							:state="Boolean(item.value)"
+							placeholder="Choose a file or drop it here..."
+							drop-placeholder="Drop file here..."
+						></b-form-file>
+					</div>
+					<div v-else-if="item.type === 'combobox'">
+						<user-combobox-vue :nininfo="nininfo" @combobox="getCombobox" />
+					</div>
+					<div v-else>
+						<b-form-input
+							:id="item.label"
+							:type="item.type"
+							v-model="item.value"
+							required
+						></b-form-input></div></b-col
+			></b-row>
+			<div class="float-right mt-2">
+				<b-button variant="success" type="submit">Submit</b-button>
+			</div>
+		</b-form>
 	</div>
 </template>
 <script>
@@ -44,12 +60,16 @@ export default {
 	components: { UserComboboxVue },
 	data() {
 		return {
+			dismissSecs: 5,
+			dismissCountDown: 0,
+			showDismissibleAlert: false,
+			ninInfo: {},
 			professional_tab: [
 				{
 					name: 'User',
 					label: 'nin_info',
 					type: 'combobox',
-					value: '',
+					value: null,
 				},
 				{
 					name: 'Name',
@@ -71,6 +91,42 @@ export default {
 				},
 			],
 		};
+	},
+	methods: {
+		countDownChanged(dismissCountDown) {
+			this.dismissCountDown = dismissCountDown;
+		},
+		showAlert() {
+			this.dismissCountDown = this.dismissSecs;
+		},
+		getCombobox(value) {
+			this.ninInfo = value;
+		},
+		async onSubmit() {
+			const data = this.professional_tab.reduce(
+				(acc, cur) => ({ ...acc, [cur.label]: cur.value }),
+				{}
+			);
+
+			const formData = new FormData();
+			formData.append('name', data.name);
+			formData.append('year_of_achievement', data.year);
+			formData.append('document.nin_info', this.ninInfo.id);
+			formData.append('document.path', data.proof_of_cert);
+			formData.append('document.type', 'Certificate');
+
+			await this.axios
+				.post(`api/professional-document/`, formData, {
+					headers: {
+						'content-type': 'multipart/form-data',
+					},
+				})
+				.then(() => {
+					this.showAlert();
+					this.$router.go();
+				})
+				.catch(() => {});
+		},
 	},
 };
 </script>
