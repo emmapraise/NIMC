101e2e1f75dfc5804abafca1925904cb046de51e
Emmanuel <emmapraise@gmail.com>
2022-07-08 14:47:59 +0100

    fix migration

--- api/migrations/0016_rename_date_of_brith_nininfo_date_of_birth.py
+++ api/migrations/0016_rename_date_of_brith_nininfo_date_of_birth.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RenameField(
             model_name="nininfo",
-            old_name="date_of_brith",
+            old_name="date_of_birth",
             new_name="date_of_birth",
         ),
     ]
